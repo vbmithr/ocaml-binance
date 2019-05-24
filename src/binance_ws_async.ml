81aa783e4715caa466a5121a3caa91bc3809a0cf
Vincent Bernardoff <vb@luminar.eu.org>
2019-05-24 18:32:16 +0200

    ws_async: add hb_ns optional arg

--- src/binance_ws_async.ml
+++ src/binance_ws_async.ml
@@ -14,17 +14,17 @@ let src = Logs.Src.create "binance.ws.async"
 
 module Log_async = (val Logs_async.src_log src : Logs_async.LOG)
 
-let connect ?(buf=Bi_outbuf.create 4096) streams =
+let connect ?(buf=Bi_outbuf.create 4096) ?hb_ns streams =
   let url = Uri.with_path url "stream" in
   let url = Uri.with_query url ["streams", [Stream.to_path streams]] in
-  Fastws_async.connect_ez url >>= fun (r, w, cleaned_up) ->
+  Fastws_async.connect_ez ?hb_ns url >>= fun (r, w, cleaned_up) ->
   Pipe.close w ;
   let client_read = Pipe.map r ~f:begin fun msg ->
       Yojson_repr.destruct_safe event_encoding (Yojson.Safe.from_string ~buf msg)
     end in
   return (client_read, cleaned_up)
 
-let with_connection ?buf streams ~f =
-  connect ?buf streams >>= fun (r, _cleaned_up) ->
+let with_connection ?buf ?hb_ns streams ~f =
+  connect ?buf ?hb_ns streams >>= fun (r, _cleaned_up) ->
   Monitor.protect (fun () -> f r)
     ~finally:(fun () -> Pipe.close_read r ; Deferred.unit)
--- src/binance_ws_async.mli
+++ src/binance_ws_async.mli
@@ -1,11 +1,12 @@
+open Core
 open Async
 
 open Binance_ws
 
 val connect :
-  ?buf:Bi_outbuf.t -> Stream.t list ->
+  ?buf:Bi_outbuf.t -> ?hb_ns:Int63.t -> Stream.t list ->
   (event Pipe.Reader.t * unit Deferred.t) Deferred.t
 
 val with_connection :
-  ?buf:Bi_outbuf.t -> Stream.t list ->
+  ?buf:Bi_outbuf.t -> ?hb_ns:Int63.t -> Stream.t list ->
   f:(event Pipe.Reader.t -> 'a Deferred.t) -> 'a Deferred.t
