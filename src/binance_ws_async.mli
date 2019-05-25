9e1f5a2b37704cc1f70270d4502db79bdf9ba1a0
Vincent Bernardoff <vb@luminar.eu.org>
2019-05-26 01:44:12 +0200

    ws_async: fix compilation

--- src/binance_ws_async.mli
+++ src/binance_ws_async.mli
@@ -4,9 +4,13 @@ open Async
 open Binance_ws
 
 val connect :
-  ?buf:Bi_outbuf.t -> ?hb_ns:Int63.t -> Stream.t list ->
+  ?buf:Bi_outbuf.t ->
+  ?hb_ns:Time_stamp_counter.Calibrator.t * Int63.t ->
+  Stream.t list ->
   (event Pipe.Reader.t * unit Deferred.t) Deferred.t
 
 val with_connection :
-  ?buf:Bi_outbuf.t -> ?hb_ns:Int63.t -> Stream.t list ->
+  ?buf:Bi_outbuf.t ->
+  ?hb_ns:Time_stamp_counter.Calibrator.t * Int63.t ->
+  Stream.t list ->
   f:(event Pipe.Reader.t -> 'a Deferred.t) -> 'a Deferred.t
