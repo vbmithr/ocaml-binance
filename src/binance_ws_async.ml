cf912e78b31b28c42ade7a0f7d06bb6d6fce30ed
Vincent Bernardoff <vb@luminar.eu.org>
2019-12-29 17:02:38 +0100

    async: remove Persistent.create'

--- src/binance_ws_async.ml
+++ src/binance_ws_async.ml
@@ -3,14 +3,5 @@ open Binance_ws
 
 let of_string ?buf msg = Yojson_repr.destruct_safe encoding (Yojson.Safe.from_string ?buf msg)
 
-module Persistent = struct
-  module Conn = Fastws_async.MakePersistent(struct type r = Binance_ws.t type w = unit end)
-  include Persistent_connection_kernel.Make(Conn)
-
-  let create' ~server_name ?on_event ?retry_delay ?buf ?hb =
-    create ~server_name ?on_event ?retry_delay ~connect:begin fun url ->
-      let rd = of_string ?buf in
-      let wr _ = "" in
-      Fastws_async.connect ?hb ~rd ~wr url
-    end
-end
+module Conn = Fastws_async.MakePersistent(struct type r = Binance_ws.t type w = unit end)
+module Persistent = Persistent_connection_kernel.Make(Conn)
--- src/binance_ws_async.mli
+++ src/binance_ws_async.mli
@@ -1,18 +1,5 @@
-open Core
-open Async
-
 val of_string : ?buf:Bi_outbuf.t -> string -> Binance_ws.t
 
-module Persistent : sig
-  include Persistent_connection_kernel.S
-    with type address = Uri.t
-     and type conn = (Binance_ws.t, unit) Fastws_async.t
-
-  val create' :
-    server_name:string ->
-    ?on_event:(Event.t -> unit Deferred.t) ->
-    ?retry_delay:(unit -> Time_ns.Span.t) ->
-    ?buf:Bi_outbuf.t ->
-    ?hb:Time_ns.Span.t ->
-    (unit -> address Or_error.t Deferred.t) -> t
-end
+module Persistent : Persistent_connection_kernel.S
+  with type address = Uri.t
+   and type conn = (Binance_ws.t, unit) Fastws_async.t
