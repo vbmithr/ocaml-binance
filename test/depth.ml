5c047cba97b8a62b0dfa080ffe52ed30725dbdac
Vincent Bernardoff <vb@luminar.eu.org>
2020-02-03 14:54:19 +0100

    fix compilation

--- test/depth.ml
+++ test/depth.ml
@@ -10,16 +10,16 @@ module Log_async = (val Logs_async.src_log src : Logs_async.LOG)
 let drop_events_before depth id =
   let _before, after =
     Depth.Set.partition begin fun { Depth.last_update_id ; _ } ->
-      last_update_id <= id
+      Int64.(last_update_id <= id)
     end depth in
   after
 
 let merge_diffs b a { Depth.bids ; Depth.asks ; _ } =
   let b = List.fold_left bids ~init:b ~f:begin fun acc { p ; q } ->
-      Map.(if q = 0. then remove acc p else set acc ~key:p ~data:q)
+      Map.(if Float.equal q 0. then remove acc p else set acc ~key:p ~data:q)
     end in
   let a = List.fold_left asks ~init:a ~f:begin fun acc { p ; q } ->
-      Map.(if q = 0. then remove acc p else set acc ~key:p ~data:q)
+      Map.(if Float.equal q 0. then remove acc p else set acc ~key:p ~data:q)
     end in
   b, a
 
@@ -68,7 +68,7 @@ let orderbook symbols init c =
           let last_update_id =
             Option.value_map (String.Map.find prev symbol) ~default:last_update_id
               ~f:(fun { last_update_id ; _ } -> last_update_id) in
-          if d.Depth.first_update_id <> Int64.succ last_update_id then
+          if Int64.(d.Depth.first_update_id <> Int64.succ last_update_id) then
             failwith "orderbook: sequence problem, aborting" ;
           let b, a = merge_diffs bids asks d in
           Pipe.write w (Some d, Some b, Some a) >>= fun () ->
@@ -85,8 +85,8 @@ let orderbook symbols init c =
               return (create_acc unprocessed bids asks)
             | Some { first_update_id ; last_update_id ; _ } ->
               (* Previous events received *)
-              if first_update_id > Int64.succ id ||
-                 last_update_id < Int64.succ id then
+              if Int64.(first_update_id > Int64.succ id ||
+                        last_update_id < Int64.succ id) then
                 failwithf "orderbook: inconsistent data received (%Ld %Ld %Ld)"
                   first_update_id last_update_id last_update_id () ;
               let prev_d, bids, asks =
